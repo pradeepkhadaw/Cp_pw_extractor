fcdfaf670f810aff5fa19c07de05b2dc4816189d
pradeepkhadaw <72299349+pradeepkhadaw@users.noreply.github.com>
2025-04-29 06:29:19 +0530

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "23000618"
 api_hash = "da88b4f9c15b2860271e086444346a93"
-bot_token = "8174804084:AAH3eNhj7cwMxHKQUkeYuFnnMDz44mdpz0U"
+bot_token = "8174804084:AAEBOn2XlqGEsvXyvsFI35w6aKTZJaqYaKY"
 auth_users = [877609705]
